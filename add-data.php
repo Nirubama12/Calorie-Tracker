0676eb87ad2c19ab5e92fdc603702f72b2d8594d
Nirubama12 <nirubama.anbu@yahoo.in>
2021-07-09 12:25:17 +0300

    h

--- add-data.php
+++ add-data.php
@@ -1,32 +1,38 @@
 <?php
 
+ 
+
 $server = "localhost";
 $user = "root";
 $pass = "";
 $db = "test";
 
+ 
+
 $conn = new mysqli($server, $user, $pass, $db);
+
+ 
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+ 
+
 if(isset ($_POST['submit']))
 {
-	$foodItem = $_POST['foodItem'];
-	$cals = $_POST['calories'];
-	$sql = "INSERT INTO calories (foodItem, calories) VALUES ('$foodItem','$cals')";
+    $foodItem = $_POST['foodItem'];
+    $cals = $_POST['calories'];
+    $sql = "INSERT INTO calories (foodItem, calories) VALUES ('$foodItem','$cals')";
 
-	if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";} 
+ 
+
+    if ($conn->query($sql) === TRUE) {
+        echo "";} 
     else {
         echo "Error: " . $sql . "<br>" . $conn->error;}
  }
-
 $conn->close();
+header("Location: https://localhost:4433/calorie-tracker/add-data.html");
+exit();
 ?>
-
-<button onclick="location.href='add-data.html'" type="button">
-         Add more Data</button>
-
-<button onclick="location.href='display-table.php'" type="button">Go to User page</button>
--- display-table.php
+++ display-table.php
@@ -1,83 +1,149 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="display-table.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital@1&display=swap" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-
- </head>
-<body>
-
- <p>Click the buttons to create and delete row(s) for the table.</p>
-
- <form id="forms">
-<label for="name">Username:</label><br>
-<input type="text" id="uname" name="name"><br><br>
-<label for="name">Select Day:</label><br>
-<select name="day" id="day">
-  <option value="sun">Sunday</option>
-  <option value="mon">Monday</option>
-  <option value="tue">Tuesday</option>
-  <option value="wed">Wednesday</option>
-  <option value="thur">Thursday</option>
-  <option value="fri">Friday</option>
-  <option value="sat">Saturday</option>
-</select>
-<br><br>
-<label for="foods">Select Food:</label>
-<select name="foods" id="foods" onchange="showCals(this.value)">
-  <?php 
-  $server = "localhost";
-  $user = "root";
-  $pass = "";
-  $db = "test";
-  
-  session_start();
-  $conn = new mysqli($server, $user, $pass, $db);
 
  
 
-  if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-  }
+</head>
+<body>
+<a href="www.google.com" target="_blank" style="font-size:50px; margin-right: 2%;width:100%";><i class="fa fa-calculator" style="margin-left: 90%;"></i></a>
+<div class="signup-container">
 
-  $sql = "SELECT * FROM calories";
-  $result = mysqli_query($conn, $sql);
-  while($row = mysqli_fetch_array($result))
-  {
-    echo '<option>'.$row['foodItem'].'</option>';
-  }
-  ?>
-</select>
-</form> 
-<br><br>
-<button onclick="myCreateFunction()">Insert</button>
-<br><br>
+ 
 
- <table id="myTable">
-  <tr>
+  <div class="left-container">
+    <h1 style="color: white; font-size:40px;">
+      <i class="fa fa-heartbeat"></i>
+      FITNESSIVE
+    </h1>
+  <div class="fitness">
+    <img src="2.png" style="height:30%; width:40%;">
+  </div>
+</div>
+
+ 
+
+ 
+
+<div class="right-container">
+  <header>
+    <h1 id="tagline">Yay! Let's get Fitter, Healthier and Happier!</h1>
+    
+<div class="set">
+<form id="forms">  
+    <div class="user-name">
+    <label for="uname">Username:</label><br>
+    <input type="text" id="uname" name="name" placeholder="Enter your username"><br><br>
+    </div>
+
+ 
+
+    <div class="day">
+    <label for="day">Select Day:</label><br>
+    <select name="day" id="day">
+    <option value="sun">Sunday</option>
+    <option value="mon">Monday</option>
+    <option value="tue">Tuesday</option>
+    <option value="wed">Wednesday</option>
+    <option value="thur">Thursday</option>
+    <option value="fri">Friday</option>
+    <option value="sat">Saturday</option>
+    </select>
+    <br><br>
+    </div>
+
+ 
+
+    <div class="food-list">
+    <label for="foods">Select Food:</label><br>
+    <select name="foods" id="foods" onchange="showCals(this.value)">    
+      
+    
+
+ 
+
+      <?php 
+      $server = "localhost";
+      $user = "root";
+      $pass = "";
+      $db = "test";
+      
+      session_start();
+      $conn = new mysqli($server, $user, $pass, $db);
+    
+          if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+          }
+    
+          $sql = "SELECT * FROM calories";
+          $result = mysqli_query($conn, $sql);
+          while($row = mysqli_fetch_array($result))
+          {
+            echo '<option>'.$row['foodItem'].'</option>';
+          }
+          ?>
+    
+
+ 
+
+
+     </select>
+     </div>
+     </form> 
+     </div>
+     <br><br>
+     <button onclick="myCreateFunction()" id="insertbtn">Insert</button>
+     <br><br>
+     </header>
+     </div>
+   </div>
+
+ 
+
+ 
+
+<table id="mytable">
+  <tr id="header-row">
     <td><b>Username</b></td>
     <td><b>Food</b></td>
     <td><b>Calories</b></td>
   </tr>
 </table>
 <br>
-<hr>
-
- <p id="total"></p>
+<div id="total-disp">
+<p id="total-text">Your Total is :</p>
+<p id="total">0</p>
+</div>
 <br><br>
 <button id="donebtn" onclick="storedaycal()">I am done for the day!</button>
 <br><br>
 <p id="idname1"></p>
 
- <script>
+ 
+
+ 
+
+ 
+
+ 
+
+
+<script>
   var sum = 0;
   var xvals = [];
   var yvals = [];
   var i = 0;
 
  
+
   function myCreateFunction() {
-  var table = document.getElementById("myTable");
-  var x = document.getElementById("myTable").rows.length;
+  var table = document.getElementById("mytable");
+  var x = document.getElementById("mytable").rows.length;
   var idname = "id"+x;
 
  
@@ -88,6 +154,7 @@
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
   cell3.id = idname;
+  row.id = "content-row";
   cell1.innerHTML = document.getElementById("uname").value;
   cell2.innerHTML = document.getElementById("foods").value;
   showCals(food, idname);
@@ -131,14 +198,17 @@ function total(vals) {
 
 function storedaycal()
 {
+
+ 
+
   var day = document.getElementById("day").value;
   var uname = document.getElementById("uname").value;
   window.location='storedaycal.php?day='+day+'&sum='+sum+'&uname='+uname;
 }
-
 </script>
 
-<button type="button" onclick="location.href='calculators.html'">Calculators!</button>
+ 
+
 </body>
 </html>
  
